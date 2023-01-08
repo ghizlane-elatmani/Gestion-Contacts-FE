034ce89c15264f6a483b1f2da6a805e989b9e920
Ghizlane <ghizlane.elatmani.99@gmail.com>
2023-01-08 20:49:43 +0100

    1.5.0: Add new component to edit a contact

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,9 +9,10 @@ import { AboutComponent } from './component/about/about.component';
 import { FormsModule } from '@angular/forms';
 import { NewContactComponent } from './component/new-contact/new-contact.component';
 import { AddContactComponent } from './component/add-contact/add-contact.component';
+import { EditContactComponent } from './component/edit-contact/edit-contact.component';
 
 @NgModule({
-  declarations: [AppComponent, ContactsComponent, AboutComponent, NewContactComponent, AddContactComponent],
+  declarations: [AppComponent, ContactsComponent, AboutComponent, NewContactComponent, AddContactComponent, EditContactComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],
