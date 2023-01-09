df41b1e9caa5ab0085f564208378e5d69fdb75af
Ghizlane <ghizlane.elatmani.99@gmail.com>
2023-01-09 09:36:25 +0100

    1.7.2: About page done

--- src/app/component/about/about.component.ts
+++ src/app/component/about/about.component.ts
@@ -6,8 +6,6 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-  infos = { name: 'Mohamed', email: 'Mohamed@gmail.com' };
-
   constructor() {}
 
   ngOnInit(): void {
